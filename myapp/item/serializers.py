069a41904cd7b2b2787b9cc00e88dd5b16397532
keeka2 <keeka2@naver.com>
2020-01-24 05:30:11 +0900

    img url field name

--- myapp/item/serializers.py
+++ myapp/item/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from .models import Item
 class ItemSerializer(serializers.ModelSerializer):
-    #imageId=serializers.SerializerMethodField()
-    imgUrl=serializers.CharField(source='imageId')
-    imgUrl=serializers.SerializerMethodField()
+    imgUrl=serializers.SerializerMethodField('get_imageId')
+
     class Meta:
         model = Item # 모델 설정
         fields = ('id','imgUrl','name','price','gender','category','ingredients','monthlySales','oilyRating','dryRating','sensitiveRating') # 필드 설정
-    def get_imgUrl(self, obj):
-        return "https://grepp-programmers-challenges.s3.ap-northeast-2.amazonaws.com/2020-birdview/thumbnail/" + obj.imgUrl + ".jpg"
+    def get_imageId(self, obj):
+        return "https://grepp-programmers-challenges.s3.ap-northeast-2.amazonaws.com/2020-birdview/thumbnail/" + obj.imageId + ".jpg"
+
 
 class ItemDetailSerializer(serializers.ModelSerializer):
     imageId = serializers.SerializerMethodField()
