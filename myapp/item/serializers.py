efa821acdc0521845e5cd4cfdd660d6ccea32d18
keeka2 <keeka2@naver.com>
2020-01-26 02:28:29 +0900

    ingredients remove(,)

--- myapp/item/serializers.py
+++ myapp/item/serializers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from .models import Item
 class ItemSerializer(serializers.ModelSerializer):
     imgUrl=serializers.SerializerMethodField('get_imageId')
-
     class Meta:
         model = Item # 모델 설정
         fields = ('id','imgUrl','name','price','ingredients','monthlySales','oilyRating','dryRating','sensitiveRating') # 필드 설정
     def get_imageId(self, obj):
         return "https://grepp-programmers-challenges.s3.ap-northeast-2.amazonaws.com/2020-birdview/thumbnail/" + obj.imageId + ".jpg"
+    def get_ingredients(self, obj):
+        return obj.ingredients[1:-1]
 
 
 class ItemDetailSerializer(serializers.ModelSerializer):
